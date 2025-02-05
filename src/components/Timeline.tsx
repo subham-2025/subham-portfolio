cffdf9922332eb4476fd657d579fe7948dc3a696
Rohit Kumar <rohit2@wisc.edu>
2025-02-05 14:11:14 -0600

    Corrected spacing in exp

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -20,7 +20,7 @@ function Timeline() {
             iconStyle={{ background: '#5000ca', color: 'rgb(39, 40, 34)' }}
             icon={<FontAwesomeIcon icon={faBriefcase} />}
           >
-            <h3 className="vertical-timeline-element-title">Google: Product Manager(Capstone Project) </h3>
+            <h3 className="vertical-timeline-element-title">Google: Product Manager (Capstone Project) </h3>
             <h4 className="vertical-timeline-element-subtitle"> Madison, WI</h4>
             <p>
             Product Development & System Design, User Experience & Interface Design, Operational Analytics & Fleet Monitoring
